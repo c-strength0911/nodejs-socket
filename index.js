143448599c29de5f6d94ebbafd1a93fd6484e6c9
shinwoo <sinwoo1225@gmail.com>
2021-07-30 20:55:14 +0900

    채팅, 닉네임 설정 단계별 로직 세팅, 가위바위보 이미지 업로드

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const socketIo = require("socket.io");
+const dotenv = require("dotenv");
+const logger = require("morgan");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+dotenv.config();
+const app = express();
+
+app.use(helmet());
+app.use(logger("dev"));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "static")));
+
+const server = app.listen(4000, () => {
+	console.log(`✅ server listen http://localhost:4000`);
+});
+
+const io = socketIo(server);
+
+const clientMap = {};
+
+io.on("connection", (socket) => {
+    console.log(`✅ Socket Connect`);
+    clientMap[socket.id] = socket;
+
+    /**
+     * Nickname Setting
+     */
+    socket.on("setNickname", (data) => {
+        socket.nickname = data.nickname;
+    });
+
+
+    /**
+     * Chat Setting
+     */
+    socket.on("sendMsg", (data) => {
+        console.log("==== SendMsg Event ====");
+        socket.broadcast.emit("sendMsg", { msg: data.msg, from: socket.nickname});
+        
+    });
+
+    /**
+     * Common Setting
+     */
+    socket.on("close", () => {
+        delete clientMap[socket.id];
+    });
+});
---
+++ static/js/main.js
@@ -0,0 +1,94 @@
+const socket = io("https://00b65a7f3f87.ngrok.io");
+
+/**
+ * Common Setting
+ */
+const nicknameBox = document.getElementById("nicknameBox");
+const chatBox = document.getElementById("chatBox");
+const LOG_S = "==========================================\n";
+const LOG_E = "\n==========================================";
+const HIDDEN_STR = "hidden"; 
+
+const hiddenAll = () =>{
+    nicknameBox.setAttribute(HIDDEN_STR, "");
+    chatBox.setAttribute(HIDDEN_STR, "");
+}
+
+/**
+ * NickName Setting
+ */
+const initNickName = () => {
+    console.log(`${LOG_S}[INIT NICKNAME PHASE]${LOG_E}`);
+    hiddenAll();
+
+    const nicknameForm = document.getElementById("nicknameForm");
+    const nicknameInput = document.getElementById("nicknameInput");
+
+    nicknameForm.addEventListener("submit", (event) => {
+        console.log("==== nickname submit ====");
+        event.preventDefault();
+
+        if(nicknameInput.value.trim().length < 2) {
+            alert("닉네임은 최소 두 글자 이상이어야 합니다 -_-;;;");
+            return;
+        }
+
+        socket.emit("setNickname", { nickname: nicknameInput.value });
+        initChat();
+    })
+
+    nicknameBox.removeAttribute(HIDDEN_STR);
+};
+
+/**
+ * Chat Setting
+ */
+const initChat = () => {
+    console.log(`${LOG_S}[CHAT PHASE]${LOG_E}`);
+    hiddenAll();
+
+    const chatMsg = document.getElementById("chatMsg");
+    const chatForm = document.getElementById("chatForm");
+    const chatTextarea = document.getElementById("chatTextarea");
+
+    const sendMsg = () => {
+        const msg = chatTextarea.value;
+        if(msg.trim().length <= 0) {
+            return;
+        }
+        
+        const msgParagraph = document.createElement("p");
+        msgParagraph.innerHTML = `<b>나</b> ${chatTextarea.value}`;
+        chatMsg.appendChild(msgParagraph);
+        socket.emit("sendMsg", { msg });
+        chatTextarea.value = "";
+    }
+
+    chatForm.addEventListener("submit", (event) => {
+        console.log("==== chat submit ====");
+        event.preventDefault();
+        sendMsg();
+    });
+    
+    chatTextarea.addEventListener("keypress", (event) => {
+        // Chat 텍스트영역에서 Enter를 누를 경우
+        if(event.keyCode === 13) {
+            event.preventDefault();
+            sendMsg();
+        }
+    });
+
+    /**
+     * socket io event 
+     */
+    socket.on("sendMsg", (data) => {
+        console.log(data);   
+        const msgParagraph = document.createElement("p");
+        msgParagraph.innerHTML = `<b>${data.from}</b> ${data.msg}`;
+        chatMsg.appendChild(msgParagraph);
+    });
+
+    chatBox.removeAttribute(HIDDEN_STR);
+};
+
+initNickName();
