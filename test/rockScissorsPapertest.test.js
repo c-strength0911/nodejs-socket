7fbb89f893f5c7f0f72694c6e7a99ce4db388076
c-strength0911 <57119447+c-strength0911@users.noreply.github.com>
2021-08-11 23:34:59 +0900

    judgeGame 완성!

--- test/rockScissorsPapertest.test.js
+++ test/rockScissorsPapertest.test.js
@@ -39,6 +39,22 @@ test("가위바위보 테스트1", () => {
   expect(result1).toBe(DRAW_STR);
   expect(result2).toBe(DRAW_STR);
   expect(result3).toBe(DRAW_STR);
+  
+test('가위바위보 테스트1', () => {
+    //given
+    const inputList1 = [ROCK_STR, ROCK_STR, ROCK_STR, ROCK_STR];
+    const inputList2 = [PAPER_STR,PAPER_STR];
+    const inputList3 = [SCISSORS_STR,SCISSORS_STR,SCISSORS_STR];
+    
+    //when
+    const result1 = judgeGame(inputList1);
+    const result2 = judgeGame(inputList2);
+    const result3 = judgeGame(inputList3);
+    
+    //then
+    expect(result1).toBe(DRAW_STR);
+    expect(result2).toBe(DRAW_STR);
+    expect(result3).toBe(DRAW_STR);
 });
 
 test("가위바위보 테스트2", () => {
